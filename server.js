d06ea3067bafd3d1a7c878403e1b3607684b7216
candelo02 <candeloj2002@gmail.com>
2024-09-11 19:41:12 -0500

    cambio aleat

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const http = require('http'); 
+const http = require('http'); // Importar el módulo http
 
+// Crear un servidor
 const server = http.createServer((req, res) => {
+  // Establecer el código de estado y el tipo de contenido de la respuesta
+  //cambiio aleat
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hola, Mundo!\n'); // Enviar la respuesta
 });
 
+// Asignar un puerto y hacer que el servidor escuche en ese puerto
 const port = 3000;
 server.listen(port, () => {
   console.log(`El servidor está escuchando en http://localhost:${port}`);
